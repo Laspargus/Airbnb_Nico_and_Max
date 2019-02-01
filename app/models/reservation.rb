5b84e335863a7ff7832e973e6fc4f14d8fd0110a
cacdesign <pro.nicolasfernandez@gmail.com>
2019-02-01 15:32:19 +0100

    ok

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,15 +1,23 @@
 class Reservation < ApplicationRecord
-	has_many :rooms
+	belongs_to :room
 	belongs_to :admin, class_name: "User"
 	belongs_to :guest, class_name: "User"
 
 	validates :start_date, :presence => {:message => "must be a valid date/time" }
   	validates :end_date, :presence => {:message => "must be a valid date/time"}
 	validate :start_must_be_before_end_time
+	validate :overlap
 
 	  def start_must_be_before_end_time
 	    errors.add(:start_date, "must be beforehihihi end time") unless
 	       start_date < end_date
+	  end
+
+	  def overlap
+	  	Self.each do |reservation|
+	  		if reservation.room == 
+	  		end_date < reservation.start_date
+	  	end
 	  end
 
 
