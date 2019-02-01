b3a9a5de15e2d91438c8a94980f9017402f50704
cacdesign <pro.nicolasfernandez@gmail.com>
2019-02-01 18:07:51 +0100

    last_version

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
 	has_many :rooms
-	validates :zip_code, uniqueness: true,  format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "please enter a valid french zip code" }
-	validates :name, presence: true,
+	#validates :zip_code, uniqueness: true,  format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "please enter a valid french zip code" }
+	#validates :name, presence: true,
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,24 +1,37 @@
 class Reservation < ApplicationRecord
+	
+@@reservations = []
+
 	belongs_to :room
 	belongs_to :admin, class_name: "User"
 	belongs_to :guest, class_name: "User"
 
-	validates :start_date, :presence => {:message => "must be a valid date/time" }
-  	validates :end_date, :presence => {:message => "must be a valid date/time"}
-	validate :start_must_be_before_end_time
-	validate :overlap
+	#validates :start_date, :presence => {:message => "must be a valid date/time" }
+  	#validates :end_date, :presence => {:message => "must be a valid date/time"}
+	#validate :start_must_be_before_end_time
+	#validate :overlap
 
 	  def start_must_be_before_end_time
 	    errors.add(:start_date, "must be beforehihihi end time") unless
 	       start_date < end_date
 	  end
 
-	  def overlap
-	  	Self.each do |reservation|
-	  		if reservation.room == room
-	  		end_date < reservation.start_date
-	  		end
-	  	end
+
+	  def initialize(id)
+	  	@@reservations << self
+	  	puts @@reservations
+	  end
+
+
+	  def self.overlap
+	  	
+	  	
+	  	puts self.first
+	  	puts self.first.start_date
+
+	  	@@reservations.each do |reservation|
+	  		puts reservation.start_date
+	  	end 
 	  end
 
 end
