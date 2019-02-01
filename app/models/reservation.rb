ad07969aefb98260f30298d4ec59efe8a283164c
cacdesign <pro.nicolasfernandez@gmail.com>
2019-02-01 15:06:42 +0100

    validates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,15 @@ class Reservation < ApplicationRecord
 	has_many :rooms
 	belongs_to :admin, class_name: "User"
 	belongs_to :guest, class_name: "User"
+
+	validates :start_date, :presence => {:message => "must be a valid date/time" }
+  	validates :end_date, :presence => {:message => "must be a valid date/time"}
+	validate :start_must_be_before_end_time
+
+	  def start_must_be_before_end_time
+	    errors.add(:start_date, "must be beforehihihi end time") unless
+	       start_date < end_date
+	  end
+
+
 end
